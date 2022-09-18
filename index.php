eed94ad1d8633c5c800ae5d7c5773e1f7a85bbd4
Vladislav _ <ageew.vlad@gmail.com>
2022-09-18 12:55:02 +0500

    itinial layout

---
+++ index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="./Resources/styles/style.css">
+    <link rel="stylesheet" href="./Resources/styles/normalize.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,400&display=swap" rel="stylesheet">
+
+    <script defer src="Resources/js/reduceHeader.js"></script>
+
+    <title>Interior.</title>
+</head>
+<body>
+
+<header>
+    <div class="header container">
+        <div class="companyName">
+            <div>
+                <a href="index.php">
+                    <img class="companyImage" src="./Resources/img/furniture.png" alt="logo">
+                </a>
+            </div>
+            <div>
+                <div class="title">Interior<span class="accent">.</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="header__links">
+            <div class="header__link index">
+                <a href="index.php">Главная</a>
+            </div>
+            <div class="header__link news">
+                <a href="news.php">Блог</a>
+            </div>
+            <div class="header__link prices">
+                <a href="prices.php">Каталог</a>
+            </div>
+            <div class="header__link contacts">
+                <a href="contacts.php">Контакты</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<main>
+
+    <section class="section__1" style="background-image: url('/Resources/img/content/bgc1.webp')">
+
+        <div class="section__filter">
+            <div class="section__absoluteTitle">
+                <h2><i>WELCOME TO INTERIOR</i></h2>
+                <h1>Modern Interior & Design</h1>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="section__2 container" >
+        <div class="benefits">
+            <div class="benefits__item">
+                <div class="benifits__img">
+                    <img src="/Resources/img/content/section2_benifits.png" alt="benefits">
+                </div>
+                <div class="benifits__text">
+                    <div class="benifits__title"> <h3><span class="accent">C</span>reative & Modern</h3></div>
+                    <div class="benifits__descr">
+                        <i>
+                            For each project we establish relationships with partners who we know will help us.
+                            <br>
+                            with partners who we know will help us.
+                        </i>
+                    </div>
+                </div>
+            </div>
+            <div class="benefits__item">
+                <div class="benifits__img">
+                    <img src="/Resources/img/content/section2_benifits.png" alt="benefits">
+                </div>
+                <div class="benifits__text">
+                    <div class="benifits__title"> <h3><span class="accent">C</span>reative & Modern</h3></div>
+                    <div class="benifits__descr">
+                        <i>
+                            For each project we establish relationships with partners who we know will help us.
+                        </i>
+                    </div>
+                </div>
+            </div>
+            <div class="benefits__item">
+                <div class="benifits__img">
+                    <img src="/Resources/img/content/section2_benifits.png" alt="benefits">
+                </div>
+                <div class="benifits__text">
+                    <div class="benifits__title"> <h3><span class="accent">C</span>reative & Modern</h3></div>
+                    <div class="benifits__descr">
+                        <i>
+                            For each project we establish relationships with partners who we know will help us.<br>
+                            with partners who we know will help us.
+                        </i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section__3" style="background-image: url('/Resources/img/content/bgc2.webp')">
+        <div class="professionals container">
+            <div class="professionals__textBox">
+                <div class="professionals__preText">
+                    OUR PROFESSIONAL SERVICES
+                </div>
+                <div class="professionals__title">
+                    <h2>We Will Create Modern And First Class Intorior<span class="accent">.</span></h2>
+                </div>
+                <div class="professionals__description">
+                    <p>
+                        Aorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.
+                    </p>
+                </div>
+            </div>
+            <div class="professionals__more">
+                <a href="news.php">DISCOVER MORE ABOUT US</a>
+            </div>
+        </div>
+    </section>
+</main>
+
+
+<footer>
+
+</footer>
+
+</body>
+</html>
